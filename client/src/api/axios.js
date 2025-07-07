039692a77cb34f260142bff63c015d425a25d9fe
Rohit Kumar <rpal46515@gmail.com>
2025-07-07 15:59:45 +0530

    Update axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -4,21 +4,13 @@ const instance = axios.create({
   baseURL: "https://moodsync-server.onrender.com",
 });
 
-instance.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (
-      error.response &&
-      error.response.status === 401 &&
-      error.response.data.detail === "Token has expired. Please login again."
-    ) {
-      localStorage.removeItem("access_token");
-      localStorage.removeItem("authUser");
-      window.location.href = "/login";
-    }
-    return Promise.reject(error);
+instance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("access_token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
   }
-);
+  return config;
+});
 
 
 export default instance;
